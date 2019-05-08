615d8d3f909a2b856e44fc92e5cb6c83021ecfd3
Mohammed Rahman <mo.rahman610@gmail.com>
2019-05-08 11:56:54 -0400

    Added View Holder

--- app/src/main/java/com/example/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/coderswag/Adapters/CategoryAdapter.kt
@@ -1,10 +1,14 @@
 package com.example.coderswag.Adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.coderswag.Model.Category
+import com.example.coderswag.R
 import kotlin.coroutines.coroutineContext
 
 class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
@@ -13,13 +17,38 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
     val categories = categories
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 
         val categoryView: View
+        val holder: ViewHolder
+
+        if (convertView == null) {
+
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder = ViewHolder()
+
+            holder.categoryImage = categoryView.findViewById(R.id.hatImage)
+            holder.categoryName= categoryView.findViewById(R.id.hatsTxt)
+            println("I exist for the first time")
+
+            categoryView.tag = holder
+        } else {
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
+
+
+
+        val category = categories[position]
+
+        val resourceID = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+        holder.categoryImage?.setImageResource(resourceID)
+        holder.categoryName?.text = category.title
+
+
+        return categoryView
     }
 
     override fun getItem(position: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 
         return categories[position]
     }
@@ -29,8 +58,12 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
     }
 
     override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 
         return categories.count()
+    }
+
+    private class ViewHolder {
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
     }
 }
--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -3,22 +3,20 @@ package com.example.coderswag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.coderswag.Adapters.CategoryAdapter
 import com.example.coderswag.Model.Category
 import com.example.coderswag.Services.DataServices
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DataServices.categories)
-
+        adapter = CategoryAdapter(this, DataServices.categories)
         categoriesListView.adapter = adapter
     }
 }
